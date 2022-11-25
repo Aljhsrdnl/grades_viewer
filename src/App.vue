d5e46d9cd0bd484f2517e08144bc1c4a560a18a0
Aljhsrdnl <blackgreen0721@gmail.com>
2022-11-25 16:53:12 +0800

    Added a footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <!-- <HomeScreen /> -->
   <router-view />
+  <footer class="text-center py-3 bg-indigo-600 text-white text-sm">
+    This is a personal project of Alejah Sardiniola. All Rights Reserved. &#xA9;
+    November 2022
+  </footer>
 </template>
 
 <script>
